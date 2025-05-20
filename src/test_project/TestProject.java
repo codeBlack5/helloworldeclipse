1f5cf8f8ff2a31ab2e92dbfb0b27ea41c25e8c87
Steve <stevekoech8@gmail.com>
2025-05-20 13:36:11 +0300

    add a variable

--- src/test_project/TestProject.java
+++ src/test_project/TestProject.java
@@ -4,7 +4,8 @@ public class TestProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is Steelo!");
+		int a = 10;
+System.out.println(a);
 	}
 
 }
